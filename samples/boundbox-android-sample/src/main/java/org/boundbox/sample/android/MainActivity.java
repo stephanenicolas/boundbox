2ae3f79ae27f4baa0a5da468617b5ec968dbcacb
Stéphane Nicolas <steff.nicolas@gmail.com>
2013-09-16 14:24:58 +0200

    Simpler android sample

--- samples/boundbox-android-sample/src/main/java/org/boundbox/sample/android/MainActivity.java
+++ samples/boundbox-android-sample/src/main/java/org/boundbox/sample/android/MainActivity.java
@@ -21,46 +21,18 @@ public class MainActivity extends Activity {
     // LIFECYCLE
     // -------------------------------
 
-    public MainActivity() {
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonMain = (Button) findViewById(R.id.button_main);
         textViewMain = (TextView) findViewById(R.id.textview_main);
-        initializeViews();
-    }
-
-    // -------------------------------
-    // PRIVATE METHODS
-    // -------------------------------
-
-    private void initializeViews() {
-        buttonMain.setOnClickListener(new OnComputeClickListener());
-    }
-
-    // protected for testing
-    protected void clickOnCompute() {
-        buttonMain.callOnClick();
-    }
-
-    // protected for testing
-    protected int getDisplayedResult() {
-        return Integer.parseInt((String) textViewMain.getText());
-    }
-
-    // -------------------------------
-    // INNER CLASSES
-    // -------------------------------
-
-    public class OnComputeClickListener implements OnClickListener {
-
-        @Override
-        public void onClick(View v) {
-            final int result = 42;
-            textViewMain.setText(String.valueOf(result));
-        }
+        buttonMain.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int result = 42;
+                textViewMain.setText(String.valueOf(result));
+            }
+        });
     }
 }
