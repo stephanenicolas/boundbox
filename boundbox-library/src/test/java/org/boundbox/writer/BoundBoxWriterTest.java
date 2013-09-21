58dd5b52e7ab413d01ab2cd42652ee2cf9ba794b
Stéphane Nicolas <steff.nicolas@gmail.com>
2013-09-21 12:59:10 +0200

    Add tests for non static fields as well. Issue #2

--- boundbox-library/src/test/java/org/boundbox/writer/BoundBoxWriterTest.java
+++ boundbox-library/src/test/java/org/boundbox/writer/BoundBoxWriterTest.java
@@ -53,7 +53,7 @@ public class BoundBoxWriterTest {
     @After
     public void tearDown() throws IOException {
         if (sandBoxDir.exists()) {
-            //FileUtils.deleteDirectory(sandBoxDir);
+            FileUtils.deleteDirectory(sandBoxDir);
         }
     }
 
@@ -70,6 +70,44 @@ public class BoundBoxWriterTest {
         FakeFieldInfo fakeFieldInfo = new FakeFieldInfo("foo", "java.lang.String");
         List<FieldInfo> listFieldInfos = new ArrayList<FieldInfo>();
         listFieldInfos.add(fakeFieldInfo);
+        classInfo.setListFieldInfos(listFieldInfos);
+        classInfo.setListConstructorInfos(Collections.<MethodInfo>emptyList());
+        classInfo.setListMethodInfos(Collections.<MethodInfo>emptyList());
+        classInfo.setListImports(new ArrayList<String>());
+
+        Writer out = createWriterInSandbox(classInfo);
+
+        // when
+        writer.writeBoundBox(classInfo, out);
+
+        // then
+        CompilationTask task = createCompileTask(classInfo, neededClasses);
+        boolean result = task.call();
+        assertTrue(result);
+
+        Class<?> clazz = loadBoundBoxClass(classInfo);
+        Method method = clazz.getDeclaredMethod("boundBox_getFoo");
+        assertNotNull(method);
+        Method method2 = clazz.getDeclaredMethod("boundBox_setFoo", String.class);
+        assertNotNull(method2);
+    }
+    
+    @Test
+    public void testProcess_class_with_many_fields() throws Exception {
+        // given
+        String classUnderTestName = "TestClassWithManyFields";
+        List<String> neededClasses = new ArrayList<String>();
+
+        ClassInfo classInfo = new ClassInfo(classUnderTestName);
+        FakeFieldInfo fakeFieldInfo = new FakeFieldInfo("foo", "java.lang.String");
+        FakeFieldInfo fakeFieldInfo2 = new FakeFieldInfo("a", "int");
+        FakeFieldInfo fakeFieldInfo3 = new FakeFieldInfo("array1", "double[]");
+        FakeFieldInfo fakeFieldInfo4 = new FakeFieldInfo("array2", "float[][]");
+        List<FieldInfo> listFieldInfos = new ArrayList<FieldInfo>();
+        listFieldInfos.add(fakeFieldInfo);
+        listFieldInfos.add(fakeFieldInfo2);
+        listFieldInfos.add(fakeFieldInfo3);
+        listFieldInfos.add(fakeFieldInfo4);
         classInfo.setListFieldInfos(listFieldInfos);
         classInfo.setListConstructorInfos(Collections.<MethodInfo>emptyList());
         classInfo.setListMethodInfos(Collections.<MethodInfo>emptyList());
