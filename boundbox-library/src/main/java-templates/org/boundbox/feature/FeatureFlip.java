a28ea50d8aa45b40c84e2fc683f0c66cee5bcdf9
Stéphane Nicolas <steff.nicolas@gmail.com>
2013-10-19 11:45:56 +0200

    Add feature flipping template and gradle build file for Android sample.

--- boundbox-library/src/main/java-templates/org/boundbox/feature/FeatureFlip.java
+++ boundbox-library/src/main/java-templates/org/boundbox/feature/FeatureFlip.java
@@ -2,6 +2,7 @@ package org.boundbox.feature;
 
 import java.util.logging.Level;
 
+//This file will be filtered with maven properties.
 public class FeatureFlip {
 
     public static final Level LOG_LEVEL = ${log.level};
